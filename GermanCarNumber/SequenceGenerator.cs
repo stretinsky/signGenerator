12447848f5a1e9fdece6bc3876ee888aa13104a8
Danis222 <danisvil@mail.ru>
2022-03-23 21:42:38 +0300

    Ну я короче ничего не сделал, наслаждайтесь

---
+++ GermanCarNumber/SequenceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GermanCarNumber
+{
+    internal class SequenceGenerator
+    {
+        private StringBuilder sequence = new StringBuilder();
+    }
+}
